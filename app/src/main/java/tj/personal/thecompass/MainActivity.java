664ad14436873a6231e17a469b267a4b2f42bede
jurekt <tomasz.jurek@snpgroup.com>
2021-05-15 12:56:39 +0200

    Fix the compass animation

--- app/src/main/java/tj/personal/thecompass/MainActivity.java
+++ app/src/main/java/tj/personal/thecompass/MainActivity.java
@@ -6,42 +6,38 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.Gravity;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManger;
-//    private lateinit var mOrientationListener: SensorEventListener
-//    private lateinit var mMagneticListener: SensorEventListener
-////    private lateinit var accelerometerValues: FloatArray
-////    private lateinit var magneticFieldValues: FloatArray
-//    private lateinit var mOrientationSensor: Sensor
-//    private var mMagneticSensor: Sensor? = null
-
-//    private lateinit var mGeomagnetic: FloatArray
+    private String TAG = this.getClass().getSimpleName();
     private float[] mGravity = new float[3];
     private float[] mGeometric = new float[3];
     private float azimuth = 0f;
     private float currentAzimuth = 0f;
     private ImageView compassImage;
-
+    private Button setDestinationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         compassImage = findViewById(R.id.compassImage);
+        setDestinationBtn = findViewById(R.id.setDestinationBtn);
         initServices();
     }
 
     private void initServices() {
         mSensorManger = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-//        mOrientationSensor = mSensorManger.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-//        mMagneticSensor = mSensorManger.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
      }
 
     @Override
@@ -66,12 +62,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         final float alpha = 0.97f;
         synchronized (this) {
             if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+//                Log.v(TAG, "ACCEL: " + sensorEvent.values[0] + " " + sensorEvent.values[1] + " " + sensorEvent.values[2]);
                 mGravity[0] = alpha * mGravity[0] + (1 - alpha) * sensorEvent.values[0];
                 mGravity[1] = alpha * mGravity[1] + (1 - alpha) * sensorEvent.values[1];
                 mGravity[2] = alpha * mGravity[2] + (1 - alpha) * sensorEvent.values[2];
             }
 
             if (sensorEvent.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+//                Log.v(TAG, "MAGNETIC: " + sensorEvent.values[0] + " " + sensorEvent.values[1] + " " + sensorEvent.values[2]);
                 mGeometric[0] = alpha * mGeometric[0] + (1 - alpha) * sensorEvent.values[0];
                 mGeometric[1] = alpha * mGeometric[1] + (1 - alpha) * sensorEvent.values[1];
                 mGeometric[2] = alpha * mGeometric[2] + (1 - alpha) * sensorEvent.values[2];
@@ -85,24 +83,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 SensorManager.getOrientation(R, orientation);
                 azimuth = (float)Math.toDegrees(orientation[0]);
                 azimuth = (azimuth + 360) % 360;
+                Log.v(TAG, String.valueOf(azimuth));
 
                 Animation animation = new RotateAnimation(-currentAzimuth, -azimuth,
                         Animation.RELATIVE_TO_SELF, 0.5f,
                         Animation.RELATIVE_TO_SELF, 0.5f);
 
+                currentAzimuth = azimuth;
                 animation.setDuration(500);
                 animation.setRepeatCount(0);
                 animation.setFillAfter(true);
 
                 compassImage.startAnimation(animation);
             }
-
         }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    public void onClickSetDestinationBtn(View view) {
+        // open prompt window
     }
 
 }
